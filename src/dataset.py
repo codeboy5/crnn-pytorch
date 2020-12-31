5a7603a13968e20e03fd0cf5c34d7f2ea0ab246c
Saksham Rastogi <saksham@pop-os.localdomain>
2020-12-31 17:43:41 +0530

    added loader for IIIT5K dataset

--- src/dataset.py
+++ src/dataset.py
@@ -8,6 +8,7 @@ from scipy.io import wavfile
 import cv2
 from PIL import Image
 import numpy as np
+import scipy.io as sio
 
 
 class Synth90kDataset(Dataset):
@@ -83,6 +84,79 @@ class Synth90kDataset(Dataset):
 
 
 def synth90k_collate_fn(batch):
+    images, targets, target_lengths = zip(*batch)
+    images = torch.stack(images, 0)
+    targets = torch.cat(targets, 0)
+    target_lengths = torch.cat(target_lengths, 0)
+    return images, targets, target_lengths
+
+
+class IIIT5KDataset(Dataset):
+    CHARS = '0123456789abcdefghijklmnopqrstuvwxyz'
+    CHAR2LABEL = {char: i + 1 for i, char in enumerate(CHARS)}
+    LABEL2CHAR = {label: char for char, label in CHAR2LABEL.items()}
+
+    def __init__(self, root_dir=None, mode=None, paths=None, img_height=32, img_width=100):
+        if root_dir and mode and not paths:
+            paths, texts = self._load_from_raw_files(root_dir, mode)
+        elif not root_dir and not mode and paths:
+            texts = None
+
+        self.paths = paths
+        self.texts = texts
+        self.img_height = img_height
+        self.img_width = img_width
+
+    def _load_from_raw_files(self, root_dir, mode):
+        
+        data_str = None
+        if mode == 'train':
+            data_str = 'traindata'
+        else :
+            data_str = 'testdata'
+        
+        data = sio.loadmat(os.path.join(root_dir, data_str + '.mat'))[data_str][0]
+        img, label = zip(*[(x[0][0], x[1][0]) for x in data])
+
+        paths = []
+        texts = []
+    
+        for i in range(len(img)) :
+            paths.append(os.path.join(root_dir,img[i]))
+            texts.append(label[i].lower())
+    
+        return paths, texts
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, index):
+        path = self.paths[index]
+
+        try:
+            image = Image.open(path).convert('L')  # grey-scale
+        except IOError:
+            print('Corrupted image for %d' % index)
+            return self[index + 1]
+
+        image = image.resize((self.img_width, self.img_height), resample=Image.BILINEAR)
+        image = np.array(image)
+        image = image.reshape((1, self.img_height, self.img_width))
+        image = (image / 127.5) - 1.0
+
+        image = torch.FloatTensor(image)
+        if self.texts:
+            text = self.texts[index]
+            target = [self.CHAR2LABEL[c] for c in text]
+            target_length = [len(target)]
+
+            target = torch.LongTensor(target)
+            target_length = torch.LongTensor(target_length)
+            return image, target, target_length
+        else:
+            return image
+
+def iiit5k_collate_fn(batch):
     images, targets, target_lengths = zip(*batch)
     images = torch.stack(images, 0)
     targets = torch.cat(targets, 0)
