979ae0a285e76326a4eaa4e58dca6edba8e25319
Saksham <sakshamdev5@gmail.com>
2021-02-15 20:25:19 +0530

    Ready To Train

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 
 common_config = {
-    'data_dir': 'data/mnt/ramdisk/max/90kDICT32px/',
+    'data_dir': 'data/UPTI',
     'img_width': 100,
     'img_height': 32,
     'map_to_seq_hidden': 64,
@@ -15,9 +15,9 @@ train_config = {
     'lr': 0.0005,
     'show_interval': 500,
     'valid_interval': 20000,
-    'save_interval': 20000,
+    'save_interval': 100,
     'cpu_workers': 4,
-    'reload_checkpoint': 'checkpoints/crnn_4_loss1.702002783203125.pt',
+    'reload_checkpoint': None,
     'valid_max_iter': 100,
     'decode_method': 'greedy',
     'beam_size': 10,
